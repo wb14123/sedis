38b53f3ef9ecba9b8cf66654e40fdac45d8bc8fc
Bin Wang <wbin00@gmail.com>
2017-01-03 11:33:39 +0800

    make netdecoder decode muliple packages

--- src/main/scala/me/binwang/sedis/NetDecoder.scala
+++ src/main/scala/me/binwang/sedis/NetDecoder.scala
@@ -5,15 +5,13 @@ import scala.util.{Failure, Success, Try}
 trait NetDecoder[T] {
   def send(data: Byte): Try[Option[T]]
 
-  def decode(data: Array[Byte]): Try[T] = {
-    var result: Try[T] = Failure(new Exception("no enough data to decode"))
-    data.foreach{byte =>
+  def decode(data: Array[Byte]): Seq[Try[T]] = {
+    data.flatMap { byte =>
       send(byte) match {
-        case Success(None) =>
-        case Failure(e) => result = Failure(e)
-        case Success(Some(r)) => result = Success(r)
+        case Success(None) => None
+        case Failure(e) => Some(Failure(e))
+        case Success(Some(r)) => Some(Success(r))
       }
     }
-    result
   }
 }
--- src/test/scala/me/binwang/sedis/RedisDecoderTest.scala
+++ src/test/scala/me/binwang/sedis/RedisDecoderTest.scala
@@ -3,37 +3,39 @@ package me.binwang.sedis
 import me.binwang.sedis.RedisProtocol._
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Success
+
 class RedisDecoderTest extends FlatSpec with Matchers {
   it should "decode simple string" in {
     val decoder = new RedisDecoder()
-    decoder.decode("+PONG\r\n".getBytes).get should equal(RedisString("PONG"))
+    decoder.decode("+PONG\r\n".getBytes).head.get should equal(RedisString("PONG"))
   }
 
   it should "decode errors" in {
     val decoder = new RedisDecoder()
-    decoder.decode("-Error Message\r\n".getBytes).get should equal(RedisError("Error Message"))
+    decoder.decode("-Error Message\r\n".getBytes).head.get should equal(RedisError("Error Message"))
   }
 
   it should "decode integers" in {
     val decoder = new RedisDecoder
-    decoder.decode(":100\r\n".getBytes).get should equal(RedisInteger(100))
+    decoder.decode(":100\r\n".getBytes).head.get should equal(RedisInteger(100))
   }
 
   it should "decode string bulks" in {
     val decoder = new RedisDecoder
-    decoder.decode("$3\r\nFOO\r\n".getBytes).get should equal(RedisString("FOO"))
+    decoder.decode("$3\r\nFOO\r\n".getBytes).head.get should equal(RedisString("FOO"))
   }
 
   it should "encode string bulks with special chars" in {
     val decoder = new RedisDecoder
-    decoder.decode("$5\r\nFOO\r\n\r\n".getBytes).get should equal(RedisString("FOO\r\n"))
+    decoder.decode("$5\r\nFOO\r\n\r\n".getBytes).head.get should equal(RedisString("FOO\r\n"))
   }
 
   it should "decode arrays" in {
     val decoder = new RedisDecoder
     decoder.decode(
       "*3\r\n$3\r\nFOO\r\n:100\r\n$3\r\nBAR\r\n".getBytes
-    ).get should equal(
+    ).head.get should equal(
       RedisArray(Seq(RedisString("FOO"), RedisInteger(100), RedisString("BAR")))
     )
   }
@@ -42,9 +44,22 @@ class RedisDecoderTest extends FlatSpec with Matchers {
     val decoder = new RedisDecoder
     decoder.decode(
       "*3\r\n$3\r\nFOO\r\n:100\r\n*1\r\n$3\r\nBAR\r\n".getBytes
-    ).get should equal(
+    ).head.get should equal(
       RedisArray(Seq(RedisString("FOO"), RedisInteger(100), RedisArray(Seq(RedisString("BAR")))))
     )
+  }
+
+  it should "decode multiple packages" in {
+    val decoder = new RedisDecoder
+    decoder.decode("$3\r\nFOO\r\n$3\r\nBAR\r\n".getBytes) should equal(
+      Seq(Success(RedisString("FOO")), Success(RedisString("BAR")))
+    )
+  }
+
+  it should "decode split package" in {
+    val decoder = new RedisDecoder
+    decoder.decode("$3\r\n".getBytes) should equal(Seq())
+    decoder.decode("FOO\r\n".getBytes).head.get should equal(RedisString("FOO"))
   }
 
 }
